1937096b4a26c573db502878ba5e352b0539ff4f
bakjonghun <bakjonghun@hikick.kr>
2021-02-13 15:15:02 +0900

    add: kickboard controller & view router

---
+++ src/controllers/kickboard.ts
@@ -0,0 +1,85 @@
+import admin from 'firebase-admin';
+import firestore from '../tools/firestore';
+
+export default class Kickboard {
+  public static async getKickboards(
+    latitude: number,
+    longitude: number
+  ): Promise<
+    {
+      id: string;
+      lat: number;
+      lng: number;
+      battery: number;
+      baseMinute: number;
+      basePrice: number;
+      extraMinute: number;
+      extraPrice: number;
+    }[]
+  > {
+    const distance = 1;
+    const results: {
+      id: string;
+      lat: number;
+      lng: number;
+      battery: number;
+      baseMinute: number;
+      basePrice: number;
+      extraMinute: number;
+      extraPrice: number;
+    }[] = [];
+    const kick = firestore.collection('kick');
+
+    const lat = 0.0144927536231884;
+    const lon = 0.0181818181818182;
+
+    const lowerLat = latitude - lat * distance;
+    const lowerLon = longitude - lon * distance;
+
+    const greaterLat = latitude + lat * distance;
+    const greaterLon = longitude + lon * distance;
+
+    const lesserGeopoint = new admin.firestore.GeoPoint(lowerLat, lowerLon);
+    const greaterGeopoint = new admin.firestore.GeoPoint(
+      greaterLat,
+      greaterLon
+    );
+
+    const kickboards = await kick
+      .where('can_ride', '==', true)
+      .where('location', '>=', lesserGeopoint)
+      .where('location', '<=', greaterGeopoint)
+      .get();
+
+    kickboards.forEach((doc) => {
+      const kickboard = doc.data();
+      if (kickboard.battery === 0) return;
+      results.push({
+        id: kickboard.name,
+        lat: kickboard.location.latitude,
+        lng: kickboard.location.longitude,
+        battery: kickboard.battery,
+        baseMinute: 5,
+        basePrice: 1200,
+        extraMinute: 1,
+        extraPrice: 180,
+      });
+    });
+
+    console.log(
+      JSON.stringify({
+        type: 'FeatureCollection',
+        features: results.map(({ lat, lng }) => ({
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat],
+          },
+        })),
+      })
+    );
+
+    return results;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+import InternalError from './tools/error';
+import Kickboard from './controllers/kickboard';
+import Wrapper from './tools/wrapper';
 import dotenv from 'dotenv';
 if (process.env.NODE_ENV === 'development') dotenv.config();
+
+export const handler = Wrapper(async (event, context, cb) => {
+  const { lat, lng } = event.queryStringParameters;
+  if (!Number(lat) || !Number(lng)) {
+    throw new InternalError('좌표를 입력해주세요.');
+  }
+
+  const kickboards = await Kickboard.getKickboards(Number(lat), Number(lng));
+  cb(null, {
+    statusCode: 200,
+    body: JSON.stringify(kickboards),
+  });
+});
