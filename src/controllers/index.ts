61e2b766a6791259319c894f49f6130a959d02af
bakjonghun <bakjonghun@hikick.kr>
2021-03-14 12:18:39 +0900

    fix: get health check

---
+++ src/controllers/index.ts
@@ -0,0 +1,1 @@
+export { default as Kickboard } from './kickboard';
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
-import InternalError from './tools/error';
-import Kickboard from './controllers/kickboard';
-import Wrapper from './tools/wrapper';
-import dotenv from 'dotenv';
-if (process.env.NODE_ENV === 'development') dotenv.config();
+import { OPCODE, Wrapper } from './tools';
 
+import { Kickboard } from './controllers';
+import dotenv from 'dotenv';
+import os from 'os';
+
+if (process.env.NODE_ENV === 'dev') dotenv.config();
+
+const hostname = os.hostname();
 export const handler = Wrapper(async (event, context, cb) => {
-  const { lat, lng } = event.queryStringParameters;
-  if (!Number(lat) || !Number(lng)) {
-    throw new InternalError('좌표를 입력해주세요.');
+  const { queryStringParameters } = event;
+  if (
+    !queryStringParameters ||
+    !Number(queryStringParameters.lat) ||
+    !Number(queryStringParameters.lng)
+  ) {
+    return cb(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        opcode: OPCODE.SUCCESS,
+        mode: process.env.NODE_ENV,
+        cluster: hostname,
+      }),
+    });
   }
 
-  const kickboards = await Kickboard.getKickboards(Number(lat), Number(lng));
+  const kickboards = await Kickboard.getKickboards(
+    Number(queryStringParameters.lat),
+    Number(queryStringParameters.lng)
+  );
+
   cb(null, {
     statusCode: 200,
     body: JSON.stringify(kickboards),
